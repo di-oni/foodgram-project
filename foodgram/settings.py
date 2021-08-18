1a7e5df415ce57a093ef3cfaae9485917385c398
Dmitry Onishchenko <dmitry.on@gmail.com>
2021-08-18 15:12:50 +0300

    5th review

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -13,7 +13,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 THUMBNAIL_DEBUG = True
 
@@ -80,23 +80,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "foodgram.wsgi.application"
 
+
 DATABASES = { 
     'default': { 
-        'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': os.environ.get('DB_NAME'), 
+        'USER': os.environ.get('POSTGRES_USER'), 
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
+        'HOST': os.environ.get('DB_HOST'), 
+        'PORT': os.environ.get('DB_PORT'), 
     } 
-}
-
-# DATABASES = { 
-#     'default': { 
-#         'ENGINE': 'django.db.backends.postgresql', 
-#         'NAME': os.environ.get('DB_NAME'), 
-#         'USER': os.environ.get('POSTGRES_USER'), 
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
-#         'HOST': os.environ.get('DB_HOST'), 
-#         'PORT': os.environ.get('DB_PORT'), 
-#     } 
-# } 
+} 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
